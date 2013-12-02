1e87cec5347cbceb55d8029cdc53a2eb3c86a439
Ceva24 <ceva24@outlook.com>
2013-12-02 00:33:19 +0000

    add stone-wall

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ This repo contains my personal attempts at some of the [Codility](http://www.cod
 
 Every solution was attempted within a 30 minute time limit, and  the code has not been cleaned up after submission. In cases where I was unhappy with the result, there's a separate file with an improved submission.
 
+Unfortunately there are some that I attempted that I didn't save the solution for; I may redo them for posterity in the future.
+
 Index
 ------------
 
@@ -18,5 +20,8 @@ Index
   </tr>
   <tr>
     <td>find-three</td><td>http://codility.com/demo/take-sample-test/helium2013</td><td>Hard</td><td>82</td>
+  </tr>
+  <tr>
+    <td>stone-wall</td><td>http://codility.com/demo/take-sample-test/sigma2012</td><td>Medium</td><td>100</td>
   </tr>
 </table>
---
+++ src/stone-wall/stone-wall.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+
+class Solution
+{
+    public int solution(int[] H)
+    {
+        // rules
+        /*
+        * so when we find two indices that are of the same height, we can use the same block, providing all the values in-between are higher.
+        *
+        * if we find a lower value, end the block.
+        
+        * idea - store a list of currently-active blocks. next interations can build upon top of these.
+        * once the height of an index is lower than an active block, remove that block from activity, and add a new block equaling the index's height
+        * to the active list.
+        
+        * are we able to accurately track the number of blocks used by doing this?
+        
+        * Must be O(N) - no inner loops.
+        * no addition or multiplication - should be safe using ints rather than longs.
+        
+        
+        
+        * extra - can we store blocks individual heights in the list rather than total height? an int tracking total height?
+        */
+        
+        int numberOfBlocks = 0;
+        int totalHeight = 0;
+        
+        final ArrayList<Integer> activeBlocks = new ArrayList<Integer>(); // depending on this being insertion-order.
+        
+        for (int i = 0; i < H.length; i++)
+        {
+            int height = H[i];
+            
+            if (totalHeight > height)
+            {
+                // remove block(s) - add another if necessary.
+                
+                for (int j = activeBlocks.size() - 1; j >= 0; j--) // first trouble - how do we scan and remove lesser blocks in O(N) time... is it still O(N)?
+                {
+                    int latestBlock = activeBlocks.get(j);
+                    activeBlocks.remove(j);
+                    totalHeight -= latestBlock;
+
+                    if ((totalHeight) <= height) // removed the latest block and now we're not too high.
+                    {
+                        // we're done.
+                        break;
+                    }
+                }
+                
+                // adding the other if necessary.
+                if (totalHeight < height)
+                {
+                    int newBlock = height - totalHeight;
+                    totalHeight += newBlock;
+                    activeBlocks.add(newBlock);
+                    numberOfBlocks++;
+                }
+                
+            }
+            else if (totalHeight == height)
+            {
+                continue;
+            }
+            else
+            {
+                int newBlock = height - totalHeight;
+                activeBlocks.add(newBlock);
+                totalHeight += newBlock;
+
+                // add a block. increase total height.
+                numberOfBlocks++;
+            }
+        }
+        
+        return numberOfBlocks;
+    }
+}
